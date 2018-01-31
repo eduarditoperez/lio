959838f97a41fa04a2526290a8c2975c68be29aa
eduarditoperez <eduardoperezleale@gmail.com>
2018-01-31 16:56:55 -0300

    We change the way in wich we compute exc_corr for libxc, now we accumulate the points first, then copy back the data to cpu, use libxc and then merge the values for energy and force in gpu

--- g2g/scalar_vector_types.h
+++ g2g/scalar_vector_types.h
@@ -62,10 +62,10 @@ namespace G2G {
 
   template<> class vec_type<float, 4> {
     private:
-      //float x, y, z, _w;
+      //float x, y, z, _w; // Cambio porque libxc necesita acceder a las componentes del vector.
 
     public:
-      float x, y, z, _w;
+      float x, y, z, _w; // Esto antes de libxc estaba en la parte privada.
 
       vec_type(void) {}
       vec_type(float x_in, float y_in, float z_in, float w_in) :
@@ -77,7 +77,7 @@ namespace G2G {
       }
 
       void operator+=(const vec_type & lo){
-      	x += lo.x, y += lo.y, z += lo.z, _w += lo._w;
+      	x += lo.x, y += lo.y, z += lo.z; //, _w += lo._w;
       }
 
   };
